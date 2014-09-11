38af27a6f5e5235c076ef2e27d0c01773eb4ccac
BhartiKodwani <bharti.15a@gmail.com>
2014-09-10 17:27:44 -0700

    Assignment1

---
+++ src/main/scala/hello/HelloWebAppplication.scala
@@ -0,0 +1,22 @@
+package hello
+
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.web.bind.annotation._
+import HelloWebApplication._
+
+object HelloWebApplication {
+
+  def main(args: Array[String]) {
+    SpringApplication.run(classOf[HelloWebApplication])
+  }
+}
+
+@Controller
+@EnableAutoConfiguration
+class HelloWebApplication {
+  @RequestMapping(Array("/"))
+  @ResponseBody
+  def home(): String = "Hello World!"
+}
